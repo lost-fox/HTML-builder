33efcc12bf0703f7ac30a3c68933749364a55469
lost-fox <Oljaburak@mail.ru>
2021-11-03 19:37:22 +0300

    feat: add solution 04

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -2,25 +2,22 @@ const fs = require('fs');
 const path = require('path');
 const readline = require('readline');
 
-const {
-   stdin: input,
-   stdout: output,
-} = require ('process');
+const { stdin: input, stdout: output } = require('process');
 
 const pathText = path.join(__dirname, 'notes.txt');
 let text = fs.createWriteStream(pathText, '');
-const rl = readline.createInterface({input, output});
+const rl = readline.createInterface({ input, output });
 
-rl.write ('Hello! Write text... \n');
+rl.write('Hello! Write text... \n');
 
 rl.on('line', (stdin) => {
-   if(stdin == 'exit'){
-      rl.close();
-   }else{
-      text.write(stdin + '\n');
-   }   
+  if (stdin == 'exit') {
+    rl.close();
+  } else {
+    text.write(stdin + '\n');
+  }
 });
 
 rl.on('close', () => {
-   console.log('Bye!');
+  console.log('Bye!');
 });
--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,22 @@
+const fs = require('fs');
+const path = require('path');
 
+const folderName = path.join(__dirname, 'files-copy');
+const folderPath = path.join(__dirname, 'files');
+
+function callback(err) {
+  if (err) throw err;
+}
+
+fs.mkdir(folderName, { recursive: true }, callback);
+fs.readdir(folderPath, (err, data) => {
+  if (err) throw err;
+  for (let i = 0; i < data.length; i++) {
+    fs.copyFile(
+      `${path.join(folderPath, data[i])}`,
+      `${path.join(folderName, data[i])}`,
+      callback
+    );
+  }
+  console.log('Copy DONE!');
+});
