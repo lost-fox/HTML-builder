53687ec63bd5a4a5f6605881873d02070828ba88
lost-fox <Oljaburak@mail.ru>
2021-11-07 23:57:45 +0300

    fix: complete solution 06

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,82 +1,68 @@
-const {fs} = require ('fs');
+const fs = require('fs');
 const path = require('path');
 
 const projectDist = path.join(__dirname, 'project-dist');
 const assetsFirst = path.join(__dirname, 'assets');
-const assetsProject = path.join(__dirname,'project-dist','assets');
+const assetsProject = path.join(__dirname, 'project-dist', 'assets');
 const styleFolder = path.join(__dirname, 'styles');
-const styleProject = path.join(projectDist,'style.css');
-const template = path.join(__dirname,'template.html');
+const styleProject = path.join(projectDist, 'style.css');
+const template = path.join(__dirname, 'template.html');
 const indexProject = path.join(projectDist, 'index.html');
-const components = path.join(__dirname,'components');
+const components = path.join(__dirname, 'components');
 
 function callback(err) {
-   if (err) throw err;
- };
+  if (err) throw err;
+}
 
- fs.mkdir(projectDist, {recursive: true}, callback);
- fs.mkdir(path.join(projectDist,'assets'), {recursive: true}, callback);
- fs.open(path.join(projectDist, 'index.html'),'w',callback);
- fs.open(path.join(projectDist, 'style.css'),'w',callback);
+fs.mkdir(projectDist, { recursive: true }, callback);
+fs.mkdir(path.join(projectDist, 'assets'), { recursive: true }, callback);
+fs.open(path.join(projectDist, 'index.html'), 'w', callback);
+fs.open(path.join(projectDist, 'style.css'), 'w', callback);
 
- fs.readdir(assetsFirst, (err,data) =>{
-    if(err) throw err;
-   for (let i=0;i<data.length;i++){
-      fs.mkdir(path.join(assetsProject, data[i]), {recursive: true}, callback);
-      fs.readdir(path.join(assetsFirst,data[i]), (err,file) =>{
-         if(err) throw err;
-         for(let j=0;j<file.length;j++){
-            fs.copyFile(`${path.join(assetsFirst,data[i],file[j])}`, `${path.join(assetsProject, data[i],file[j])}`,callback);
-         }         
-      });
-    }
- });
+fs.readdir(assetsFirst, (err, data) => {
+  if (err) throw err;
+  for (let i = 0; i < data.length; i++) {
+    fs.mkdir(path.join(assetsProject, data[i]), { recursive: true }, callback);
+    fs.readdir(path.join(assetsFirst, data[i]), (err, file) => {
+      if (err) throw err;
+      for (let j = 0; j < file.length; j++) {
+        fs.copyFile(
+          `${path.join(assetsFirst, data[i], file[j])}`,
+          `${path.join(assetsProject, data[i], file[j])}`,
+          callback
+        );
+      }
+    });
+  }
+});
 
-fs.readdir(styleFolder, (err,file) =>{
-   if(err) throw err;
-   for(let i=0;i<file.length;i++){
-      fs.readFile(path.join(styleFolder,file[i]), (err,data) =>{
-         if(err) throw err;
-         fs.appendFile(styleProject, data, callback);
-      });
-   }
+fs.readdir(styleFolder, (err, file) => {
+  if (err) throw err;
+  for (let i = 0; i < file.length; i++) {
+    fs.readFile(path.join(styleFolder, file[i]), (err, data) => {
+      if (err) throw err;
+      fs.appendFile(styleProject, data, callback);
+    });
+  }
 });
 
 const stream = new fs.ReadStream(template, 'utf-8');
 
 stream.on('readable', () => {
-   const data = stream.read();
-   if(data !=null){
-      fs.writeFile(indexProject,data,callback);
-   }
+  let data = stream.read();
+  const index = fs.createReadStream(indexProject, 'utf-8');
+  if (data != null) {
+    fs.readdir(components, (err, file) => {
+      if (err) throw err;
+      for (let i = 0; i < file.length; i++) {
+        let name = `{{${file[i].slice(0, file[i].lastIndexOf('.'))}}}`;
+        let regex = new RegExp(name, 'gi');
+        fs.readFile(path.join(components, file[i]), 'utf-8', (err, comp) => {
+          if (err) throw err;
+          data = data.replace(regex, comp);
+          fs.writeFile(indexProject, data, callback);
+        });
+      }
+    });
+  }
 });
-
-fs.readdir(components , (err,file)=>{
-   if(err) throw err;
-   for(let i=0;i<file.length;i++){
-      let name = `{{${file[i].slice(0,file[i].lastIndexOf('.'))}}}`;
-     let regex = new RegExp (name,'gi');
-      fs.readFile(path.join(components,file[i]),'utf-8', (err,comp) =>{
-         if(err) throw err;
-         let indexText = '';
-         const index = new fs.createReadStream (indexProject,'utf-8');
-
-         index.on('data', (chunk)=>{
-            indexText += chunk.toString().replace(regex,comp);
-         });
-         index.on('end', () => {
-            fs.writeFile(indexProject,indexText, callback);
-         });
-         // fs.readFile(indexProject, 'utf-8', (err,index) =>{
-         //    if(err) throw err;
-            
-         //   indexText = index.replace(regex, comp);
-         //   //index = indexText;
-         //  //fs.appendFile(indexProject,indexText,callback);
-         //   // console.log(index); 
-         // });
-        
-      });      
-   }
-});
-
